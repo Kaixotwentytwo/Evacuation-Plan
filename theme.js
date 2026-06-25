a252966591c8523468758758b7d7195a4ea88e03
Kaixotwentytwo <101523181+Kaixotwentytwo@users.noreply.github.com>
2026-06-25 09:49:24 +0300

    Delete theme.js
    changing structure of app

--- theme.js
+++
@@ -1,9 +0,0 @@
-const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
-
-mediaQuery.addEventListener('change', (event) => {
-    if (event.matches) {
-        console.log("Switched to Dark Mode");
-    } else {
-        console.log("Switched to Light Mode");
-    }
-});
